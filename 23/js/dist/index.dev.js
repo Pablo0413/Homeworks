c6417c233f9b707dc8db2b828c0ec94269dcb514
Pablo0413 <71309446+Pablo0413@users.noreply.github.com>
2022-09-04 15:41:28 +0300

    Final JS

--- 23/js/dist/index.dev.js
+++ 23/js/dist/index.dev.js
@@ -2,9 +2,17 @@
 
 // Моя норма 1 сезон Атаки Титанів в день
 var userName = prompt('Enter your name');
-console.log('Hello, ' + userName); // Моя норма 2 літри випитої кожного дня
+alert("Hello, ".concat(userName)); // Моя норма 2 літри випитої води кожного дня
 
-var userAge = prompt('Enter your '); // Моя норма 3 прочитані книги на три місяці
+var currentYear = 2022;
+var userYearBirth = prompt("Enter your Year of birth");
+
+if (userYearBirth > 0) {
+  alert("Your age is ".concat(currentYear - userYearBirth));
+} else {
+  alert('Incorrect!');
+} // Моя норма 3 прочитані книги на три місяці
+
 
 var sideLenght = prompt('Enter the length of the side of the square');
 
@@ -19,6 +27,26 @@ var circleRadius = prompt('Please, insert circle radius here');
 var circleRadiusConverted = parseFloat(circleRadius);
 
 if (circleRadiusConverted > 0) {
-  var circleSquare = Math.sqrt(Math.PI * circleRadiusConverted);
-  alert("Circle square equals to: ".concat(circleSquare));
+  var circleSquare = Math.PI * Math.pow(circleRadiusConverted, 2);
+  alert("Circle square equals to: ".concat(circleSquare.toFixed(2)));
 } // Максимум 2
+
+
+var distance = parseFloat(prompt("Enter distance between cities"));
+var travelTime = parseFloat(prompt("Enter travel time"));
+
+if (distance > 0 && travelTime > 0) {
+  alert("Your speed ".concat(distance / travelTime, " km/h"));
+} else {
+  alert("Incorrect!");
+} // Максимум 3 
+
+
+var cashCount = parseFloat(prompt("How many dollars do you exchange in euros?"));
+var exchangeRate = 1;
+
+if (cashCount > 0) {
+  alert("You will get ".concat(cashCount * exchangeRate, " euros"));
+} else {
+  alert('Incorrect!');
+}
--- 23/js/index.js
+++ 23/js/index.js
@@ -1,9 +1,17 @@
 // Моя норма 1 сезон Атаки Титанів в день
 let userName = prompt('Enter your name');
-console.log('Hello, ' + userName)
+alert(`Hello, ${userName}`)
 
-// Моя норма 2 літри випитої кожного дня
-let userAge = prompt('Enter your ')
+// Моя норма 2 літри випитої води кожного дня
+const currentYear = 2022;
+let userYearBirth = prompt(`Enter your Year of birth`);
+
+if (userYearBirth > 0) {
+    alert(`Your age is ${currentYear - userYearBirth}`);
+}
+else {
+    alert('Incorrect!');
+}
 
 // Моя норма 3 прочитані книги на три місяці
 let sideLenght = prompt('Enter the length of the side of the square')
@@ -19,7 +27,28 @@ let circleRadius = prompt('Please, insert circle radius here');
 let circleRadiusConverted = parseFloat(circleRadius);
 
 if(circleRadiusConverted > 0) {
-    const circleSquare = Math.sqrt((Math.PI * circleRadiusConverted));
-    alert(`Circle square equals to: ${circleSquare}`);
+    const circleSquare = (Math.PI * (circleRadiusConverted**2));
+    alert(`Circle square equals to: ${circleSquare.toFixed(2)}`);
 }
+
 // Максимум 2
+let distance = parseFloat (prompt(`Enter distance between cities`));
+let travelTime = parseFloat (prompt(`Enter travel time`));
+
+if (distance > 0 && travelTime > 0) {
+    alert(`Your speed ${distance / travelTime} km/h`);
+}
+else {
+    alert (`Incorrect!`);
+}
+
+// Максимум 3 
+let cashCount = parseFloat(prompt(`How many dollars do you exchange in euros?`));
+const exchangeRate = 1
+
+if (cashCount > 0) {
+    alert (`You will get ${cashCount * exchangeRate} euros`);
+}
+else {
+    alert('Incorrect!');
+}
