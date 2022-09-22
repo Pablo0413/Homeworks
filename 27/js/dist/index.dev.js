6a44d0168e46b62960812f74407b355bb4736db4
Pablo0413 <71309446+Pablo0413@users.noreply.github.com>
2022-09-22 10:52:20 +0300

    Adding objects

---
+++ 27/js/dist/index.dev.js
@@ -0,0 +1,121 @@
+"use strict";
+
+/* 
+Створи масив «Список покупок». Кожен елемент масиву є об'єктом, 
+який містить назву продукту, кількість і куплений він чи ні, ціну за одиницю товару, сума. 
+Написати кілька функцій для роботи з таким масивом:
+*/
+var myShoppingList = [{
+  productName: 'Iphone 14 Pro Max',
+  quantity: 1,
+  isbought: true,
+  price: 1099.99,
+  amountPrice: 1099.99
+}, {
+  productName: 'AirPods 2',
+  quantity: 2,
+  isbought: true,
+  price: 149.35,
+  amountPrice: 298.70
+}, {
+  productName: 'MacBook Air M2 chip',
+  quantity: 1,
+  isbought: false,
+  price: 1199.99,
+  amountPrice: 1199.99
+}, {
+  productName: 'Apple Watch Series 8',
+  quantity: 1,
+  isbought: false,
+  price: 399.45,
+  amountPrice: 399.45
+}, {
+  productName: 'MagSafe Charger',
+  quantity: 4,
+  isbought: true,
+  price: 39.00,
+  amountPrice: 156.00
+}];
+/* Min 1.1  Виводити весь список на екран таким чином, щоб спочатку йшли продукти, 
+'що ще не придбані, 
+а потім - ті, що вже придбали. */
+
+function showShoppingList(list) {
+  for (var i = 0; i < list.length; i++) {
+    if (!list[i].isbought) {
+      console.log(list[i].productName);
+    }
+  }
+
+  for (var _i = 0; _i < list.length; _i++) {
+    if (list[_i].isbought) {
+      console.log(list[_i].productName);
+    }
+  }
+} // Min 1.2 Покупка продукту. Функція приймає назву продукту і відзначає його як придбаний.
+
+
+function productIsBought(productName) {
+  myShoppingList.forEach(function (element) {
+    if (productName === element.productName) {
+      element.isbought = true;
+    }
+  });
+}
+
+showShoppingList(myShoppingList);
+productIsBought('MacBook Air M2 chip');
+console.log(myShoppingList);
+/* 
+Видалення продукту зі списку (видалення повинно проводитися шляхом створення нового масиву, 
+в якому продукт, що ми шукаємо, буде відсутнім)
+*/
+
+function newProductList(productName) {
+  // filter створює новий масив з усіма елементами, яку проходять певну перевірку, яка задається у функції
+  return myShoppingList.filter(function (element) {
+    return element.productName !== productName;
+  });
+}
+
+var myNewProductList = newProductList('AirPods 2');
+console.log(myNewProductList);
+/* 
+Додавання покупки в список. 
+Враховуй, що при додаванні покупки з уже існуючим в списку продуктом, 
+необхідно збільшувати кількість в існуючій покупці, а не додавати нову. 
+При цьому також повинна змінитися сума, наприклад, якщо ціна за одиницю 12, 
+а кількості товарів стало 2, то сума буде 24.
+*/
+
+function addingToShoppingList(newProduct) {
+  var index = myShoppingList.findIndex(function (element) {
+    return element.productName === newProduct.productName;
+  });
+
+  if (index !== -1) {
+    myShoppingList[index].quantity += 1;
+    myShoppingList[index].amountPrice += myShoppingList[index].price;
+  } else {
+    myShoppingList.push(newProduct);
+  }
+}
+
+var newProduct = {
+  productName: "Apple Home",
+  quantity: 1,
+  isbought: true,
+  price: 39.00,
+  amountPrice: 39.00
+};
+addingToShoppingList(newProduct);
+console.log(myShoppingList);
+newProduct = {
+  productName: "Apple Watch Ultra",
+  quantity: 1,
+  isbought: true,
+  price: 450,
+  amountPrice: 450
+};
+addingToShoppingList(newProduct);
+console.log(myShoppingList);
